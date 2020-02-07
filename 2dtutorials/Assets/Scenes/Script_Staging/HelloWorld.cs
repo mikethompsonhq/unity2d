0daa628a65b16e90488de1fe4f1be8d112e20da4
Michael Thompson <CDA-ADLLAB@forces.gc.ca>
2020-02-07 15:43:11 -0500

    latest

---
+++ 2dtutorials/Assets/Scenes/Script_Staging/HelloWorld.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public new string name = "Untitled";
+    public bool alive;
+    public int age = 10;
+    public int[] points = new int[5];
+    public string[] names;
+
+    public float speed
+    {
+        get
+        {
+            return sm.speed;
+        }
+    }
+
+    // Start is called before the first frame update
+
+    private SpeedManager sm;
+
+    void Awake()
+    {
+        sm = gameObject.GetComponent<SpeedManager>();
+    }
+    
+    
+    void Start()
+    {
+        print("Start");
+    }
+
+    void FixedUpdate()
+    {
+       
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        transform.position = new Vector3(pos.x + speed, pos.y, pos.z);
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
---
+++ 2dtutorials/Assets/Scenes/Script_Staging/SpeedManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedManager : MonoBehaviour
+{
+    [Range(0.0f, 0.5f)]
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
